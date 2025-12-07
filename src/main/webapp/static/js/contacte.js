6171cdd1cae32515bd68247f1013de43835835d0
Martí Carreras <edu@applicationsdev.com>
2025-12-07 18:23:35 +0100

    Versió 1.1.2: Afegir formulari de contacte

---
+++ src/main/webapp/static/js/contacte.js
@@ -0,0 +1,32 @@
+
+window.addEventListener(
+    'load',
+    function () {
+        let envia = document.getElementById('envia');
+        envia.addEventListener(
+            'click',
+            function () {
+                let nom = document.getElementById('nom');
+                let email = document.getElementById('email');
+                let missatge = document.getElementById('missatge');
+                if (
+                    nom.value.trim() === ''
+                    || email.value.trim() === ''
+                    || missatge.value.trim() === ''
+                ) {
+                    alert('Nom, Email i Missatge són camps obligatoris.');
+                } else {
+                    nom.value = '';
+                    email.value = '';
+                    missatge.value = '';
+                    setTimeout(
+                        function () {
+                            alert('Missatge enviat.');
+                        },
+                        1000
+                    );
+                }
+            }
+        );
+    }
+);
